41e929cb8c01bf1c5a5010a04e37060c6c8e0cd3
JiongnanLiu <jia7833@hotmail.com>
2021-05-28 22:59:36 -0400

    Add friendships model

---
+++ friendships/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Friendship(models.Model):
+    from_user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='following_friendship_set',
+    )
+    to_user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='follower_friendship_set',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        index_together = (
+            # select all my followings, ordered by following time
+            ('from_user_id', 'created_at'),
+            # select all my followers, ordered by followed time
+            ('to_user_id', 'created_at'),
+        )
+
+        unique_together = (('from_user_id', 'to_user_id'),)
+
+    def __str__(self):
+        return f'{self.from_user_id} followed {self.to_user_id}'
